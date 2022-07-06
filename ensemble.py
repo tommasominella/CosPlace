196e26369f130b0f0f5d7ec2e4063ccae5df8e49
tommasominella <105786187+tommasominella@users.noreply.github.com>
2022-07-06 10:40:20 +0200

    Update ensemble.py

--- ensemble.py
+++ ensemble.py
@@ -3,18 +3,27 @@ import logging
 import torchvision
 from torch import nn
 
+from model.layers import Flatten, L2Norm, 
 
 class MyEnsemble(nn.Module):
     def __init__(self, firstModel, secondModel):
         super(MyEnsemble, self).__init__()
         self.firstModel = modelA
         self.secondModel = modelB
-        self.classifier = nn.Linear(in_features, n_classes) #define accordingly
-        self.relu = nn.ReLU()
+        
+        
+        self.aggregation = nn.Sequential(
+                L2Norm(),
+                GeM(),
+                Flatten(),
+                nn.Linear(features_dim, fc_output_dim),
+                L2Norm()
+            )
+    
 
     def forward(self, x1, x2):
         x1 = self.firstModel(x1)
         x2 = self.secondModel(x2)
         x = torch.cat((x1, x2), dim=1)
-        x = self.classifier(self.relu(x))
+        x = self.aggregation(x)
         return x
